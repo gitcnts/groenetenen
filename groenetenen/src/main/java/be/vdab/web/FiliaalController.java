0eb2631c572c7dc988d57de8803218af44092041
Carina.Nuyts <nuyts.carine@gmail.com>
2017-03-03 15:16:42 +0100

    1ste commit

---
+++ groenetenen/src/main/java/be/vdab/web/FiliaalController.java
@@ -0,0 +1,23 @@
+package be.vdab.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/filialen")
+class FiliaalController {
+	private static final String FILIALEN_VIEW = "filialen/filialen"; // JSP
+	private static final String TOEVOEGEN_VIEW = "filialen/toevoegen"; // JSP
+
+	@GetMapping
+	String findAll() {
+		return FILIALEN_VIEW;
+	}
+
+	@GetMapping("toevoegen")
+	String createForm() {
+		return TOEVOEGEN_VIEW;
+	}
+
+}
