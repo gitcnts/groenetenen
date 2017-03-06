f55983b3c43c7e6a3f528f5a3f59aaf165b58d9d
Carina.Nuyts <nuyts.carine@gmail.com>
2017-03-06 10:54:53 +0100

    Na hoofdstuk 12

--- groenetenen/src/main/java/be/vdab/web/FiliaalController.java
+++ groenetenen/src/main/java/be/vdab/web/FiliaalController.java
@@ -1,7 +1,10 @@
 package be.vdab.web;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 class FiliaalController {
 	private static final String FILIALEN_VIEW = "filialen/filialen"; // JSP
 	private static final String TOEVOEGEN_VIEW = "filialen/toevoegen"; // JSP
+	private static final String REDIRECT_URL_NA_TOEVOEGEN = "redirect:/filialen";
+	private static final Logger LOGGER = Logger.getLogger(FiliaalController.class.getName());
+	// importeer Logger uit de package java.util.logging
 
 	@GetMapping
 	String findAll() {
@@ -18,6 +24,13 @@ class FiliaalController {
 	@GetMapping("toevoegen")
 	String createForm() {
 		return TOEVOEGEN_VIEW;
+	}
+
+	@PostMapping
+	String create() {
+		// later voeg je een record toe aan de database
+		LOGGER.info("filiaal record toevoegen aan database");
+		return REDIRECT_URL_NA_TOEVOEGEN;
 	}
 
 }
