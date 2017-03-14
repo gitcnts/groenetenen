40d1628269eed7796b5cff4357bcc03920f7daa7
Carine Nuyts <nuyts.carine@gmail.com>
2017-03-14 16:00:44 +0100

    Na hoofdstuk 26

--- groenetenen/src/main/java/be/vdab/web/Initializer.java
+++ groenetenen/src/main/java/be/vdab/web/Initializer.java
@@ -5,12 +5,13 @@ import javax.servlet.Filter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import be.vdab.datasource.DataSourceConfig;
 import be.vdab.repositories.RepositoriesConfig;
 import be.vdab.services.ServicesConfig;
 
 // enkele imports
 public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-	
+
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
@@ -18,7 +19,8 @@ public class Initializer extends AbstractAnnotationConfigDispatcherServletInitia
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] { RepositoriesConfig.class, ServicesConfig.class };
+		return new Class<?>[] { DataSourceConfig.class, RepositoriesConfig.class, ServicesConfig.class };
+
 	}
 
 	@Override
@@ -30,5 +32,5 @@ public class Initializer extends AbstractAnnotationConfigDispatcherServletInitia
 	protected Filter[] getServletFilters() {
 		return new Filter[] { new CharacterEncodingFilter("UTF-8") };
 	}
-	
+
 }
