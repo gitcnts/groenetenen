b584a5b695583568ab7206202fdd65dbc808452a
Carina.Nuyts <nuyts.carine@gmail.com>
2017-03-15 15:18:09 +0100

    Na hoofdstuk 28

--- groenetenen/src/main/java/be/vdab/repositories/RepositoriesConfig.java
+++ groenetenen/src/main/java/be/vdab/repositories/RepositoriesConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
 @ComponentScan
@@ -21,5 +22,10 @@ public class RepositoriesConfig {
 	NamedParameterJdbcTemplate namedJdbcTemplate(DataSource dataSource) {
 		return new NamedParameterJdbcTemplate(dataSource);
 	}
-	
+
+	@Bean
+	DataSourceTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+
 }
--- groenetenen/src/main/java/be/vdab/services/DefaultFiliaalService.java
+++ groenetenen/src/main/java/be/vdab/services/DefaultFiliaalService.java
@@ -3,14 +3,12 @@ package be.vdab.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.stereotype.Service;
-
 import be.vdab.entities.Filiaal;
 import be.vdab.exceptions.FiliaalHeeftNogWerknemersException;
 import be.vdab.repositories.FiliaalRepository;
 import be.vdab.valueobjects.PostcodeReeks;
 
-@Service
+@ReadOnlyTransactionalService
 class DefaultFiliaalService implements FiliaalService {
 	private final FiliaalRepository filiaalRepository;
 
@@ -19,6 +17,7 @@ class DefaultFiliaalService implements FiliaalService {
 	}
 
 	@Override
+	@ModifyingTransactionalServiceMethod
 	public void create(Filiaal filiaal) {
 		filiaalRepository.create(filiaal);
 	}
@@ -29,11 +28,13 @@ class DefaultFiliaalService implements FiliaalService {
 	}
 
 	@Override
+	@ModifyingTransactionalServiceMethod
 	public void update(Filiaal filiaal) {
 		filiaalRepository.update(filiaal);
 	}
 
 	@Override
+	@ModifyingTransactionalServiceMethod
 	public void delete(long id) {
 		if (filiaalRepository.findAantalWerknemers(id) != 0) {
 			throw new FiliaalHeeftNogWerknemersException();
